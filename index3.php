c266f1ab6e5f97c5944de1ce83b2aadbde243f6d
lucas999-cyber <75287223+lucas999-cyber@users.noreply.github.com>
2021-04-27 19:38:44 -0300

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,55 @@
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php
+  echo "ejercicio 1 <br>";
+  echo "<br>";
+  $i=1;
+  while ($i <= 100) {
+    echo "$1-";
+          $i++;
+    // code...
+  }
+  echo "ejercicio 2<br>";
+  for ($f=100; $f >=1; $f--) {
+    echo "$f-";
+    // code...
+  }
+
+   echo "Ejercicio 3 <br>";
+
+   $i=1;
+  para ($i=1; $i <= 100;  $i++){
+      if($i % 2==0){
+          eco "$i-";
+      }
+  }
+  eco"<h3>Ejercicio 4.</h3>";
+   $i=1;
+  para ($i=1; $i <= 100;  $i++){
+      if($i % 3==0){
+          eco "$i-";
+      }
+  }
+  eco"<h3>Ejercicio 5.</h3>";
+   $suma=0;
+  para ($i = 1;  $i <= 20;  $i++) {
+      echo ($suma +=  $i), "-";
+  }
+
+  eco"<h3>Ejercicio 6.</h3>";
+   $suma=0;
+  para ($i = 1;  $i <= 20;  $i++) {
+      if($i % 2==0){
+          echo ($suma +=  $i), "-";
+      }
+  }
+
+
+   ?>
+ </body>
+ </html>
